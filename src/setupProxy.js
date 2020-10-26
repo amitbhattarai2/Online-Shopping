eec3b0f3147f567dbad11ed6c03394be613694f3
Amit Bhattarai <amit.bhattarai46@gmail.com>
2020-10-25 23:59:12 -0700

    added proxy middleware

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,5 @@
 const proxy = require('http-proxy-middleware')
 
 module.exports = (app) => {
-  app.use(
-    proxy('/api/*', { target: 'http://onlineshoppingappn.herokuapp.com/' })
-  )
+  app.use(proxy('/*', { target: 'http://onlineshoppingappn.herokuapp.com' }))
 }
