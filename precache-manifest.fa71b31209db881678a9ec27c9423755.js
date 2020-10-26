1496eaaa1384b3fc1a46a028b4c865f1961e6d91
Amit Bhattarai <amit.bhattarai46@gmail.com>
2020-10-25 23:10:17 -0700

    Updates

---
+++ precache-manifest.fa71b31209db881678a9ec27c9423755.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68005a766acd52ad911e20c62ed3d71b",
+    "url": "/Online-Shopping/index.html"
+  },
+  {
+    "revision": "6c52c0f5d65700b50808",
+    "url": "/Online-Shopping/static/css/main.df742129.chunk.css"
+  },
+  {
+    "revision": "cb30d2195be74e2325b1",
+    "url": "/Online-Shopping/static/js/2.7d217226.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Online-Shopping/static/js/2.7d217226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c52c0f5d65700b50808",
+    "url": "/Online-Shopping/static/js/main.23a5fe6c.chunk.js"
+  },
+  {
+    "revision": "1c39bde477bfd3d9700e",
+    "url": "/Online-Shopping/static/js/runtime-main.ec29d2e1.js"
+  }
+]);
