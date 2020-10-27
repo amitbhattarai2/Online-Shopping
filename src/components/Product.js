4745b49a5e07d7f6cafcf57bcfc62b019133d136
Amit Bhattarai <amit.bhattarai46@gmail.com>
2020-10-27 15:18:24 -0700

    Changes to package.json proxy

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Card } from 'react-bootstrap'
-import Rating from './Rating'
+//import Rating from './Rating'
 
 const Product = ({ product }) => {
   return (
